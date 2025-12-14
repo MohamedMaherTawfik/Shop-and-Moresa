d0a43a4f73320ce9953fd5a3a0de7bb73bcbeb53
MohamedMaherTawfik <m7mdellham77@gmail.com>
2025-12-14 20:38:35 +0200

    ..

--- app/Http/Controllers/Web/AssociationAdminAuth.php
+++ app/Http/Controllers/Web/AssociationAdminAuth.php
@@ -16,7 +16,6 @@ class AssociationAdminAuth extends Controller
     public function login_submit(Request $request)
     {
         $credentials = $request->only('email', 'password');
-        dd($credentials);
         if(Auth::attempt($credentials))
         {
             dd(Auth::user());
