123269a272f26b15abb15cef4eede101aedbba03
MohamedMaherTawfik <m7mdellham77@gmail.com>
2025-12-14 14:26:14 +0200

    add admin association

--- resources/views/layouts/admin/partials/_side-bar.blade.php
+++ resources/views/layouts/admin/partials/_side-bar.blade.php
@@ -946,13 +946,13 @@
                                     <span class="text-truncate">{{ translate('employees') }}</span>
                                 </a>
                             </li>
-
+{{--
                                <li class="nav-item">
                                 <a class="nav-link {{ (Request::is('admin/associate-admin/list') || Request::is('admin/employee/add') || Request::is('admin/employee/update*')) ? 'active' : '' }}"
                                    href="{{ route('admin.admin-list') }}" title="{{ translate('Association Admin') }}">
                                     <span class="text-truncate">{{ translate('Association Admin') }}</span>
                                 </a>
-                            </li>
+                            </li> --}}
                         </ul>
                     </li>
                 @endif
